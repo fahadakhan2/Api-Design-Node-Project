86d14ccf3fa354b73617b75f2f3cfb78d4f97842
Fahad Khan <kanlifahad@gmail.com>
2024-04-18 14:00:16 -0500

    Create our routes in router.ts and then link router to our app in index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import express from 'express'
+import express from "express";
+import router from "./router";
 
-const app = express()
+const app = express();
 
 // respond to a get request with a route of /
 // app.[method]([route], [route handler]) is the syntax
-app.get('/', (req, res) => {
-  console.log('hello from express')
-  res.status(200)
-  res.json({message : 'hello'})
-})
+app.get("/", (req, res) => {
+  console.log("hello from express");
+  res.status(200);
+  res.json({ message: "hello" });
+});
 
-export default app
+app.use("/api", router);
+
+export default app;
