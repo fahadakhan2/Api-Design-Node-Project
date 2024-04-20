40e49dd62ac010ad2652274a6213328171507ee2
Fahad Khan <kanlifahad@gmail.com>
2024-04-20 13:16:42 -0500

    Added config files for local, prod, and testing

---
+++ src/config/prod.ts
@@ -0,0 +1,3 @@
+export default {
+  port: 5002 // process.env.PORT
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
+import config from './config'
 import app from './server'
 
 // creates and starts a server for our API on a defined port
-app.listen(3001, () => {
-  console.log('hello on http://localhost:3001')
+app.listen(config.port, () => {
+  console.log(`hello on http://localhost:${config.port}`)
 })
